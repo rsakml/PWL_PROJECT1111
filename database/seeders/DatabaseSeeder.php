f76c625bdf5bab341d5f06931a9f9497c302bee3
= <dellajannata2112@gmail.com>
2022-06-05 22:23:17 +0700

    Menambahkan Data EmployeeSeeder dan Supplier Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(EmployeeSeeder::class);
+        $this->call(SupplierSeeder::class);
     }
 }
